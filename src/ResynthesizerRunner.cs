cd9ff8edc8b847ffecf2c98a9b0469feca025adf
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2023-01-28 17:11:46 -0700

    Remove an empty comment line

--- src/ResynthesizerRunner.cs
+++ src/ResynthesizerRunner.cs
@@ -222,7 +222,6 @@ namespace ContentAwareFill
             using (IDeviceContext deviceContext = d2dFactory.CreateBitmapDeviceContext(this.sourceMask))
             {
                 // PrimitiveBlend.Copy is required for Direct2D mask to out the original selection.
-                //
                 deviceContext.PrimitiveBlend = PrimitiveBlend.Copy;
 
                 using (ISolidColorBrush selectedRegionBrush = deviceContext.CreateSolidColorBrush(Colors.White))
